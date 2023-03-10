fe2861ad318343eca326ba72ddec3a6fe8084b9d
Akmal <magchuz@pm.me>
2023-03-10 18:41:14 +0700

    Move hCaptcha script to hCaptcha.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -11,7 +11,7 @@ const config = (env, argv) =>
   merge(common, {
     entry: {
       popup: PATHS.src + '/popup.js',
-      contentScript: PATHS.src + '/contentScript.js',
+      hcaptcha: PATHS.src + '/hcaptcha.js',
       background: PATHS.src + '/background.js',
     },
     resolve: {
